9ef1d4a08800f8a674647643718f531f7f516ace
Arthur Hsiao <xiawpohr@gmail.com>
2019-04-13 15:53:20 +0800

    update the main example

--- example/main.js
+++ example/main.js
@@ -1,25 +1,35 @@
 const Web3 = require('web3')
 const ERC725DID = require('../build/lib/erc725did.min.js').default
 
-main() 
+main()
 
 async function main () {
   const web3 = new Web3('http://127.0.0.1:8545')
-  const did = new ERC725DID({ web3 })
-
+  const erc725did = new ERC725DID({ web3 })
   const accounts = await web3.eth.getAccounts()
-  const options = {
+
+  const identity = await erc725did.register({
     from: accounts[0],
     gas: 3000000
-  }
-  const identity = await did.register(options)
-  const id = await identity.getDid()
-  // const identity = await did.connect(did)
-  // const doc = await identity.resolve()
-  console.log(id)
-  // const event = await identity.addKey(key, type, purpose, options)
-  // const event = await identity.removeKey(key, options)
-  // const event = await identity.revoke(options)
+  })
+  const did = await identity.getDid()
+  console.log(did)
+
+  const identity2 = await erc725did.connect(did)
+  const did2 = await identity2.getDid()
+  console.log(did2)
+
+  const doc = await identity.resolve()
+  console.log(doc)
+
+  const key = web3.utils.keccak256(accounts[1])
+  const addKeyEvent = await identity.addKey(key, 2, 1)
+  console.log(addKeyEvent)
+
+  const removeKeyEvent = await identity.removeKey(key, 2)
+  console.log(removeKeyEvent)
+
+  await identity.revoke()
 }
 
 // did.setProvider()
